2df8b4c6bc7f87d70333387091f9c1b473685609
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2025-08-14 16:54:39 +0200

    feat: add syntax highlighting support for a comprehensive range of languages/formats/etc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,27 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "vim", "vimdoc" },
+      ensure_installed = {
+	-- Programming languages.
+	"javascript", "typescript", "rust", "go", "lua", "c", "cpp", "c_sharp", "java", "bash",
+	-- Markup languages.
+	"html", "markdown", "markdown_inline", 
+	-- Template and component syntaxes.
+	"tsx",
+	-- Styling languages.
+	"css",
+	-- Data & config formats.
+	"json", "yaml", "toml", 
+	-- Domain-specific.
+        "vim", "vimdoc", "dockerfile", "gitignore",
+      },
       auto_install = true,
       highlight = { enable = true },
+      indent = {
+        enable = true,
+        -- We use tree-sitter instead of `:h syntax`, so we deduplicate highlighting.
+        additional_vim_regex_highlighting = false,
+      },
     })
   end,
 }
