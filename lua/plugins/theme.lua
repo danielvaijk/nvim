5e524569b7d7332634d19a58ec119c14020bfa91
Daniel van Dijk <daniel@vandijk.sh>
2025-08-15 11:27:53 +0200

    fix: make background transparent

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,9 +4,15 @@ return {
     priority = 1000,
     options = {
         flavour = "mocha",
+        transparent_background = true,
     },
     config = function(_, options)
         require("catppuccin").setup(options)
         vim.cmd.colorscheme("catppuccin")
+
+        -- Additional transparency for the background.
+        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
     end,
 }
