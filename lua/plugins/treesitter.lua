f20fb9961d3dbb0e931036a62f537cb85ef898be
Daniel van Dijk <daniel@vandijk.sh>
2025-08-15 11:27:05 +0200

    refactor: apply StyLua across entire repo

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,26 +4,42 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
-            -- Programming languages.
-            "javascript", "typescript", "rust", "go", "lua", "c", "cpp", "c_sharp", "java", "bash",
-            -- Markup languages.
-            "html", "markdown", "markdown_inline", 
-            -- Template and component syntaxes.
-            "tsx",
-            -- Styling languages.
-            "css",
-            -- Data & config formats.
-            "json", "yaml", "toml", 
-            -- Domain-specific.
-            "vim", "vimdoc", "dockerfile", "gitignore",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = {
-            enable = true,
-            -- We use tree-sitter instead of `:h syntax`, so we deduplicate highlighting.
-            additional_vim_regex_highlighting = false,
-        },
-    });
+                -- Programming languages.
+                "javascript",
+                "typescript",
+                "rust",
+                "go",
+                "lua",
+                "c",
+                "cpp",
+                "c_sharp",
+                "java",
+                "bash",
+                -- Markup languages.
+                "html",
+                "markdown",
+                "markdown_inline",
+                -- Template and component syntaxes.
+                "tsx",
+                -- Styling languages.
+                "css",
+                -- Data & config formats.
+                "json",
+                "yaml",
+                "toml",
+                -- Domain-specific.
+                "vim",
+                "vimdoc",
+                "dockerfile",
+                "gitignore",
+            },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = {
+                enable = true,
+                -- We use tree-sitter instead of `:h syntax`, so we deduplicate highlighting.
+                additional_vim_regex_highlighting = false,
+            },
+        })
     end,
-};
+}
