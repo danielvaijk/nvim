02a20bfd5fcb404303e5f4af152573a53e9da9b8
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2025-08-16 17:12:15 +0200

    feat: map keys for quick file saving and quitting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,11 @@
 local telescope = require("telescope.builtin")
 
+-- File operations.
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+vim.keymap.set("n", "<leader>wq", "<cmd>wq<cr>", { desc = "Save file and quit" })
+
+-- Telescope.
 vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
