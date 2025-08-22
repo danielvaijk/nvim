67c055f4ae7d68740d5095e3ff66de68d2a79c13
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2025-08-22 16:23:33 +0200

    fix: show hidden files/directories in Telescope find_files picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,13 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("telescope").setup({
+            pickers = {
+                find_files = {
+                    hidden = true, -- Show hidden files/directories.
+                },
+            },
+        })
+    end,
 }
