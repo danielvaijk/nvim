357e7e3f21c37b9a36e384f52d8c3417990fecc8
Daniel van Dijk <daniel@stentorian.io>
2025-08-25 21:19:16 +0200

    refactor: apply new indentation style with stylua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    checker = { enabled = false }, -- Manual updates only.
+  spec = {
+    { import = "plugins" },
+  },
+  checker = { enabled = false }, -- Manual updates only.
 })
