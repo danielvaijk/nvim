c06b94301eb47e940488ded28917c7efd014bb93
Daniel van Dijk <daniel@stentorian.io>
2025-08-25 21:19:16 +0200

    refactor: apply new indentation style with stylua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,45 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                -- Programming languages.
-                "javascript",
-                "typescript",
-                "rust",
-                "go",
-                "lua",
-                "c",
-                "cpp",
-                "c_sharp",
-                "java",
-                "bash",
-                -- Markup languages.
-                "html",
-                "markdown",
-                "markdown_inline",
-                -- Template and component syntaxes.
-                "tsx",
-                -- Styling languages.
-                "css",
-                -- Data & config formats.
-                "json",
-                "yaml",
-                "toml",
-                -- Domain-specific.
-                "vim",
-                "vimdoc",
-                "dockerfile",
-                "gitignore",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = {
-                enable = true,
-                -- We use tree-sitter instead of `:h syntax`, so we deduplicate highlighting.
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        -- Programming languages.
+        "javascript",
+        "typescript",
+        "rust",
+        "go",
+        "lua",
+        "c",
+        "cpp",
+        "c_sharp",
+        "java",
+        "bash",
+        -- Markup languages.
+        "html",
+        "markdown",
+        "markdown_inline",
+        -- Template and component syntaxes.
+        "tsx",
+        -- Styling languages.
+        "css",
+        -- Data & config formats.
+        "json",
+        "yaml",
+        "toml",
+        -- Domain-specific.
+        "vim",
+        "vimdoc",
+        "dockerfile",
+        "gitignore",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = {
+        enable = true,
+        -- We use tree-sitter instead of `:h syntax`, so we deduplicate highlighting.
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
 }
