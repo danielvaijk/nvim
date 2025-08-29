8e99e4dee982cd11cc21252caabaf3f5ff41b5b1
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2025-08-29 21:33:46 +0200

    feat: configure Prettier for general formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,21 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      jsx = { "prettier" },
+      tsx = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      markdown = { "prettier" },
+      ["markdown.mdx"] = { "prettier" },
+      yaml = { "prettier" },
+      yml = { "prettier" },
+      xml = { "prettier" },
+      svg = { "prettier" },
     },
     format_on_save = {
       timeout_ms = 500,
