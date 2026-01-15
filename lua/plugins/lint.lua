06d14a228d8b1cf33b4170063d3886f99077bb41
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2026-01-15 16:14:17 +0100

    fix: linting setup

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,16 +1,23 @@
 return {
   "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("lint").linters_by_ft = {
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      vue = { "eslint_d" },
+      svelte = { "eslint_d" },
     }
 
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    vim.env.ESLINT_D_PPID = vim.fn.getpid()
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
       callback = function()
-        require("lint").try_lint()
+        lint.try_lint()
       end,
     })
   end,
